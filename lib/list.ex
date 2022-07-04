be4c5436b8abc3604de9d82b3c39e30687b531d6
Boyd Multerer <boyd@kry10.com>
2022-07-04 14:08:23 +1200

    split_columns should be part of the List module

--- lib/list.ex
+++ lib/list.ex
@@ -1,5 +1,23 @@
 defmodule Adept.List do
 
+
+  #--------------------------------------------------------
+  @doc """
+  Splits the list into n new lists organized into columns
+
+  ## Example
+      iex> Adept.Map.split_columns( [1,2,3,4,5,6,7,8,9], 3 )
+      [[1,4,7], [2,5,8], [3,6,9]]
+  """
+  @spec split_columns( items :: list, count :: pos_integer ) :: list
+  def split_columns( items, count ) when is_list(items) do
+    Enum.reduce(1..count, [], fn(c, acc) ->
+      [ Enum.take_every(Enum.drop(items, c - 1), count) | acc ]
+    end)
+    |> Enum.reverse()
+  end
+
+
   #--------------------------------------------------------
   @doc """
   Randomize the order of a list.
--- test/list_test.exs
+++ test/list_test.exs
@@ -3,6 +3,30 @@ defmodule Adept.ListTest do
     
   @len 1000
 
+  #--------------------------------------------------------
+  test "split_columns splits evenly" do
+    assert Adept.List.split_columns([1,2,3,4,5,6,7,8,9], 3) == [
+      [1,4,7],
+      [2,5,8],
+      [3,6,9],
+    ]
+  end
+
+  test "split_columns splits with leftovers" do
+    assert Adept.List.split_columns([1,2,3,4,5,6,7,8,], 3) == [
+      [1,4,7],
+      [2,5,8],
+      [3,6],
+    ]
+  end
+
+  test "split_columns makes empty columns where there aren't enough items" do
+    assert Adept.List.split_columns([1,2], 3) == [
+      [1],
+      [2],
+      []
+    ]
+  end
 
   #--------------------------------------------------------
   test "randomize randomizes a list" do
