96a3f31abd602f829c15f0e57fc09da712541725
Boyd Multerer <boyd@kry10.com>
2022-07-06 17:17:57 +1200

    add more metadata to mix.exs in order to have it look nice in hex

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Adept.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/adept-bits/adept"
+
+
   def project do
     [
       app: :adept,
-      version: "0.1.0",
-      elixir: "~> 1.13",
+      version: @version,
+      description: "A collection of light and simple utility functions for Elixir",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -25,4 +32,22 @@ defmodule Adept.MixProject do
       # {:benchee, "~> 1.1", only: :dev}
     ]
   end
+
+  defp package do
+    %{
+      licenses: ["Apache-2.0"],
+      maintainers: ["Boyd Multerer"],
+      links: %{"GitHub" => @url}
+    }
+  end
+
+  def docs do
+    [
+      extras: ["README.md"],
+      source_ref: "v#{@version}",
+      source_url: @url,
+      main: "readme"
+    ]
+  end
+
 end
